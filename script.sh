e00d96352a63af07a707c1d6080053d9a8862cfc
Patryk Konior <patryk@arteia.co>
2018-01-04 14:54:25 +0100

    fixing script

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
-sh dockerhub/build.sh
-sh dockerhub/push.sh
+sh ./dockerhub/build.sh
+sh ./dockerhub/push.sh
 npm run infra
 heroku git:remote -a ${HEROKU_APP_NAME}-staging
 git push heroku master
