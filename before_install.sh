eab7564d69c38cf0a8a19adca058311fe0c06ca3
Patryk Konior <patryk@arteia.co>
2018-01-04 13:05:08 +0100

    fixing

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-- openssl aes-256-cbc -K $encrypted_b7b87b4dd2e2_key -iv $encrypted_b7b87b4dd2e2_iv -in .netrc.enc -out .netrc -d
-- sudo cat .netrc > ~/.netrc
+openssl aes-256-cbc -K $encrypted_b7b87b4dd2e2_key -iv $encrypted_b7b87b4dd2e2_iv -in .netrc.enc -out .netrc -d
+sudo cat .netrc > ~/.netrc
