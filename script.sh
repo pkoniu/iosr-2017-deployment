311a62c46b636917fb0b02ebc3b0ab42e01f84f5
Patryk Konior <patryk@arteia.co>
2018-01-04 15:26:46 +0100

    fixing script

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 set -e
 
 pwd
-sh ${DEPLOYMENT_SCRIPTS_PATH}/dockerhub/build.sh
-sh ${DEPLOYMENT_SCRIPTS_PATH}/dockerhub/push.sh
+sh $(pwd)/../../iosr-2017-deployment/dockerhub/build.sh
+sh $(pwd)/../../iosr-2017-deployment/dockerhub/push.sh
 npm run infra
 heroku git:remote -a ${HEROKU_APP_NAME}-staging
 git push heroku master
