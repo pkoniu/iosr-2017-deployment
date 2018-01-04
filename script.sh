3056fc43560a16ff764b79fff82f90a48902d383
Patryk Konior <patryk@arteia.co>
2018-01-04 15:35:17 +0100

    fixing script -v3

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sh sh $(find ~/ | grep iosr-2017-deployment/dockerhub/push.sh)
+sh $(find ~/ | grep iosr-2017-deployment/dockerhub/push.sh)
 sh $(pwd)/../../iosr-2017-deployment/dockerhub/push.sh
 npm run infra
 heroku git:remote -a ${HEROKU_APP_NAME}-staging
