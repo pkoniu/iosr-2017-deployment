c954c034f745f1ab440979752e8aa6b7c0009756
Patryk Konior <patryk@arteia.co>
2018-01-04 15:37:35 +0100

    fixing script -v4

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
+sh $(find ~/ | grep iosr-2017-deployment/dockerhub/build.sh)
 sh $(find ~/ | grep iosr-2017-deployment/dockerhub/push.sh)
-sh $(pwd)/../../iosr-2017-deployment/dockerhub/push.sh
 npm run infra
 heroku git:remote -a ${HEROKU_APP_NAME}-staging
 git push heroku master
